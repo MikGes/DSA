dc41941ea3b84dbd122a2f50da3037a836e4c28c
MikGes <mikiyasgetnet6@gmail.com>
2026-03-26 12:46:25 +0300

    solved the palindrome linked list problem

---
+++ 234.palindrome-linked-list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        temp = head
+        arr = []
+        while temp:
+            arr.append(temp.val)
+            temp = temp.next
+        return arr == arr[::-1]
